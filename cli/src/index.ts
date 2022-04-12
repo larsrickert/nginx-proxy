14b57467b4571bf442ea1bbb7d300dab404322e1
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-04-12 16:01:52 +0200

    feat: add `--folder` and `--root` option

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -18,6 +18,14 @@ program
 		'Root directory where the applications should be deployed',
 		`${process.cwd()}/applications`
 	)
+	.option(
+		'-f, --folder <folder>',
+		'Folder name inside the applications directory that the repo is cloned to. If not provided, repository name will be used'
+	)
+	.option(
+		'-r, --root <root>',
+		'Root directory in the git repository where the docker-compose.yml is located that should be deployed. If not provided, repository root (.) will be used'
+	)
 	.action(async (url: string, options: OptionValues) => {
 		if (!shell.which('git') || !shell.which('docker-compose')) {
 			program.error('Sorry, this action requires git and docker-compose');
@@ -25,6 +33,16 @@ program
 
 		const branch = (options.branch as string).trim();
 		let applicationsDir = options.dir as string;
+		const folderName = options.folder as string | undefined;
+		const root = options.root as string | undefined;
+
+		if (folderName && (folderName.startsWith('.') || folderName.includes('/'))) {
+			return program.error(`Folder name "${folderName}" is invalid`);
+		}
+
+		if (root && root.startsWith('..')) {
+			return program.error(`Root parameter must not start with ".."`);
+		}
 
 		if (options.dir === '.') {
 			return program.error('Applications directory must not be "."');
@@ -41,7 +59,7 @@ program
 		shell.echo(`Deploying git repository "${url}" with branch "${branch}"...\n`);
 
 		try {
-			const repoPath = await cloneOrPullRepo(url, applicationsDir);
+			const repoPath = await cloneOrPullRepo(url, applicationsDir, folderName);
 			shell.cd(repoPath);
 
 			shell.echo(`Checking out branch ${branch}...`);
@@ -50,7 +68,7 @@ program
 			}
 
 			shell.echo(`Rebuilding docker-compose.yml...`);
-			if (shell.exec(` docker-compose up -d --build`).code !== 0) {
+			if (shell.exec(`${root ? `cd ${root} &&` : ''} docker-compose up -d --build`).code !== 0) {
 				program.error(`Error: docker-compose rebuild failed`);
 			}
 
--- cli/src/utils/git.ts
+++ cli/src/utils/git.ts
@@ -4,22 +4,26 @@ import shell from 'shelljs';
 import { program } from '..';
 import { exists } from './fs';
 
-export async function cloneOrPullRepo(url: string, path: string): Promise<string> {
-	const repoName = basename(url);
-	const repoPath = join(path, repoName);
+export async function cloneOrPullRepo(
+	url: string,
+	path: string,
+	folderName?: string
+): Promise<string> {
+	folderName ||= basename(url);
+	const repoPath = join(path, folderName);
 
 	await mkdir(path, { recursive: true });
 
 	if (!(await exists(repoPath))) {
-		shell.echo(`Repository "${repoName}" is deployed for the first time. Cloning repository.`);
-		if (shell.exec(`cd ${path} && git clone ${url}`).code !== 0) {
+		shell.echo(`Application "${folderName}" is deployed for the first time. Cloning repository.`);
+		if (shell.exec(`cd ${path} && git clone ${url} ${folderName}`).code !== 0) {
 			program.error(`Error: Git clone repository failed`);
 		}
 	} else {
 		const { stdout: gitUrl } = shell.exec(`cd ${repoPath} && git config --get remote.origin.url`);
 
 		if (gitUrl.replace('\n', '') !== url) {
-			program.error(`A different repository with name "${repoName}" does already exist`);
+			program.error(`A different application with name "${folderName}" does already exist`);
 		}
 
 		shell.echo('Pulling latest git changes.');
