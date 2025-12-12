f23b01fe5cfeb3c4268fc2889957c4d5372e50d1
Lars Rickert <dev@lars-rickert.de>
2025-12-12 12:52:16 +0100

    add onyx vitepress theme

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,6 @@
+import OnyxTheme from "@sit-onyx/vitepress-theme";
+
+// if you use/override custom styles, they must be imported after the theme, e.g.
+// import "./styles.css";
+
+export default OnyxTheme;
